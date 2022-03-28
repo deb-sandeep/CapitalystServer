8cf8ea2c754900710897fbf7aa83de4c60b6099c
Sandeep Deb <deb.sandeep@gmail.com>
2022-03-28 15:44:08 +0530

    Baseline

--- src/main/resources/static/views/admin/js/directive.js
+++ src/main/resources/static/views/admin/js/directive.js
@@ -1,6 +1,25 @@
-/*
-capitalystNgApp.directive( '<directive name>', function() {
-    return function( $scope, element, attributes ) {
-    } ;
-}) ;
-*/
+capitalystNgApp.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.key === 'Enter' ) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter, {'event': event});
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+capitalystNgApp.directive('ngEscape', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.key === 'Escape') {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEscape, {'event': event});
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
--- src/main/resources/static/views/admin/js/manage_ledger_categories_controller.js
+++ src/main/resources/static/views/admin/js/manage_ledger_categories_controller.js
@@ -1,3 +1,9 @@
+function CatName( name ) {
+    
+    this.displayName = name ;
+    this.beingEdited = false ;
+}
+
 capitalystNgApp.controller( 'ManageLedgerCategoriesController', 
     function( $scope, $http ) {
     
@@ -7,7 +13,8 @@ capitalystNgApp.controller( 'ManageLedgerCategoriesController',
     $scope.$parent.navBarTitle = "Manage L1 and L2 ledger categories" ;
     $scope.$parent.activeModuleId = "cat_management" ;
     
-    $scope.activeCategory = "Credit" ;
+    $scope.activeCategory = "Debit" ;
+    $scope.l1CatNameBeingEdited = null ;
     
     $scope.ledgerCategories = {
        credit : {
@@ -45,6 +52,63 @@ capitalystNgApp.controller( 'ManageLedgerCategoriesController',
         $scope.activeCategory = "Debit" ;
     }
     
+    $scope.saveCategoryAttributes = function( cat ) {
+        console.log( "TODO: Updating category " + cat ) ;
+    }
+    
+    $scope.editCategory = function( cat ) {
+        console.log( "Editing category " + cat ) ;
+    }
+    
+    $scope.editL1CategoryName = function( catName ) {
+        catName.beingEdited = true ;
+        $scope.l1CatNameBeingEdited = catName.displayName ;
+    }
+    
+    $scope.revertEditChanges = function( catName ) {
+        console.log( "Reverting edit changes" ) ;
+        catName.beingEdited = false ;
+        $scope.l1CatNameBeingEdited = null ;
+    }
+    
+    $scope.saveDebitL1CategoryName = function( catName ) {
+        
+        var oldCatName = catName.displayName ;
+        var newCatName = $scope.l1CatNameBeingEdited ;
+        
+        console.log( "Saving change in L1 category name" ) ;
+        console.log( "   Old cat name = " + oldCatName ) ;
+        console.log( "   New cat name - " + newCatName ) ;
+        
+        if( oldCatName == newCatName || newCatName == "" ) {
+            catName.beingEdited = false ;
+        }
+        else {
+            catName.beingEdited = false ;
+            
+            var l2CatMap = $scope.ledgerCategories.debit.l2Categories ;
+            var l2List   = l2CatMap.get( oldCatName ) ;
+            
+            for( var i=0; i<l2List.length; i++ ) {
+                var cat = l2List[i] ;
+                cat.l1CatName = newCatName ;
+            }
+            
+            saveCategoryEditChangesOnServer( l2List,
+                function() {
+                    l2CatMap.delete( oldCatName ) ;
+                    l2CatMap.set( newCatName, l2List ) ;
+                    
+                    catName.displayName = $scope.l1CatNameBeingEdited ;
+                    $scope.l1CatNameBeingEdited = null ;
+                },
+                function() {
+                    $scope.l1CatNameBeingEdited = null ;
+                } 
+            ) ;
+        }
+    }
+    
     // --- [END] Scope functions
 
     // -----------------------------------------------------------------------
@@ -73,6 +137,29 @@ capitalystNgApp.controller( 'ManageLedgerCategoriesController',
         }) ;
     }
     
+    function saveCategoryEditChangesOnServer( l2List, 
+                                              successCallback, 
+                                              errorCallback ) {
+        
+        $scope.$emit( 'interactingWithServer', { isStart : true } ) ;
+        $http.post( '/Ledger/Categories', l2List )
+        .then ( 
+            function( response ){
+                console.log( "Changes successfully saved on server" ) ;
+                successCallback() ;
+            }, 
+            function( error ){
+                $scope.$parent.addErrorAlert( "Error saving changes on server.\n" + 
+                                              error.data.message ) ;
+                console.log( error ) ;
+                errorCallback() ;
+            }
+        )
+        .finally(function() {
+            $scope.$emit( 'interactingWithServer', { isStart : false } ) ;
+        }) ;
+    }
+    
     // ------------------- Server response processors ------------------------
     function populateMasterCategories( categories ) {
         
@@ -84,6 +171,8 @@ capitalystNgApp.controller( 'ManageLedgerCategoriesController',
         
         for( var i=0; i<categories.length; i++ ) {
             var category = categories[i] ;
+            category.beingEdited = false ;
+            
             if( category.creditClassification ) {
                 classifyCategoryInMasterList( 
                         $scope.ledgerCategories.credit.l1Categories, 
@@ -101,17 +190,17 @@ capitalystNgApp.controller( 'ManageLedgerCategoriesController',
     
     function classifyCategoryInMasterList( l1CatList, l2CatMap, category ) {
         
-        var l1 = category.l1CatName ;
+        var catName = new CatName( category.l1CatName ) ;
         
-        if( l1CatList.indexOf( l1 ) == -1 ) {
-            l1CatList.push( l1 ) ;
+        if( l1CatList.filter( e => e.displayName == catName.displayName ).length <= 0 ) {
+            l1CatList.push( catName ) ;
         }
         
-        if( !l2CatMap.has( l1 ) ) {
-            l2CatMap.set( l1, [] ) ;
+        if( !l2CatMap.has( catName.displayName ) ) {
+            l2CatMap.set( catName.displayName, [] ) ;
         }
         
-        var l2List = l2CatMap.get( l1 ) ;
+        var l2List = l2CatMap.get( catName.displayName ) ;
         l2List.push( category ) ;
     }
         
