47209f97e04e3e715b9a12d020e60e68b2c5c2d1
Sandeep Deb <deb.sandeep@gmail.com>
2022-04-04 14:50:55 +0530

    Refactoring

--- src/main/resources/static/views/admin/js/route.js
+++ src/main/resources/static/views/admin/js/route.js
@@ -1,11 +1,11 @@
 capitalystNgApp.config( function( $routeProvider ) {
     $routeProvider 
     .when( "/", {
-        templateUrl : "/views/admin/template/manage_ledger_categories.html",
+        templateUrl : "/views/admin/template/categories/manage_ledger_categories.html",
         controller : "ManageLedgerCategoriesController"
     })
     .when( "/manage_ledger_categories", {
-        templateUrl : "/views/admin/template/manage_ledger_categories.html",
+        templateUrl : "/views/admin/template/categories/manage_ledger_categories.html",
         controller : "ManageLedgerCategoriesController"
     })
 });
