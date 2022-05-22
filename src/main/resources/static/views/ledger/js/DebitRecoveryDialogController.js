0ec09ba3878e23946f15a45d6cb2e9ebfffcbbc4
Sandeep Deb <deb.sandeep@gmail.com>
2022-05-22 18:15:38 +0530

    Baseline

--- src/main/resources/static/views/ledger/js/DebitRecoveryDialogController.js
+++ src/main/resources/static/views/ledger/js/DebitRecoveryDialogController.js
@@ -17,6 +17,9 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
     $scope.debitTxnList = [] ;
     $scope.currentResultPage = -1 ;
     
+    $scope.creditAmtRemaining = 0 ;
+    $scope.errorMessages = [] ;
+    
     // -----------------------------------------------------------------------
     // --- [START] Controller initialization ---------------------------------
     console.log( "Loading DebitRecoveryDialogController." ) ;
@@ -29,10 +32,8 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
         $( '#debitRecoveryDialog' ).modal( 'hide' ) ;
     }
 
-    $scope.$on( 'creditTxnSetForDebitRecoveryDialog', function( event, args ) {
-        
-        $scope.creditTxn = args ;
-        initializeController() ;
+    $scope.$on( 'creditTxnSetForDebitRecoveryDialog', function( event, creditTxn ) {
+        initializeController( creditTxn ) ;
     } ) ;
     
     $scope.getNextBatchOfDebitTxns = function() {
@@ -58,7 +59,7 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
         
         var entry = new SelectedDebitTxn( debitTxn, 
                                           -1,
-                                          getRemainingAmount( debitTxn ), 
+                                          getMaxRecoverableAmount( debitTxn ), 
                                           "" ) ;
                                           
         $scope.selectedDebitTxns.push( entry ) ;
@@ -66,6 +67,52 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
     
     $scope.removeDebitTxn = function( index ) {
         $scope.selectedDebitTxns.splice( index, 1 ) ;
+        // TODO: Remove it from the server if the id is != -1
+    }
+    
+    $scope.recomputeRemainingCreditAmt = function() {
+        
+        $scope.creditAmtRemaining = $scope.creditTxn.amount ;
+        
+        for( var i=0; i<$scope.selectedDebitTxns.length; i++ ) {
+            var debit = $scope.selectedDebitTxns[i] ;
+            $scope.creditAmtRemaining -= debit.recoveredAmount ;    
+        }
+    }
+    
+    $scope.isValidAssociation = function() {
+        
+        $scope.errorMessages.length = 0 ;
+        
+        validateZeroAssociations() ;
+        validateNegativeAssociationAmounts() ;
+        validateRecoveredAmountLessThanCreditAmount() ;
+        
+        return $scope.errorMessages.length == 0 ;
+    }
+    
+    $scope.showErrorMessages = function() {
+        
+        var content = "<ul>"
+        for( var i=0; i<$scope.errorMessages.length; i++ ) {
+            content += "<li>" + $scope.errorMessages[i] + "</li>" ;
+        }
+        content += "</ul>" ;
+        
+        $ngConfirm({
+            title: 'Association errors',
+            content: content,
+            scope: $scope,
+            buttons: {
+                ok: {
+                    text: 'Ok',
+                    btnClass: 'btn-default',
+                    action: function(){
+                        return true ;
+                    }
+                },
+            }
+        });
     }
     
     // --- [END] Scope functions
@@ -74,21 +121,31 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
     // --- [START] Local functions -------------------------------------------
     function clearState() {
         
+        $scope.creditTxn = null ;
+        $scope.creditAmtRemaining = 0 ;
         $scope.currentResultPage = -1 ;
         $scope.selectedDebitTxns.length = 0 ;
         $scope.debitTxnList.length = 0 ;
     }
     
-    function initializeController() {
+    function initializeController( creditTxn ) {
         
         clearState() ;
         
+        $scope.creditTxn = creditTxn ;
+        $scope.creditAmtRemaining = $scope.creditTxn.amount ;
+        
         // Fetch any already associated debit transactions
         getAssociatedDebitTxns( $scope.creditTxn.id, function(){
+            
+            for( var i=0; i<$scope.selectedDebitTxns.length; i++ ) {
+                var txn = $scope.selectedDebitTxns[i] ;
+                $scope.creditAmtRemaining -= txn.recoveredAmount ;
+            }
+            
             // Fetch the first batch of debit transactions
             $scope.getNextBatchOfDebitTxns() ;
         }) ;
-        
     }
     
     function getAssociatedDebitTxns( creditTxnId, callback ) {
@@ -145,7 +202,7 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
         )
     }
     
-    function getRemainingAmount( debitTxn ) {
+    function getMaxRecoverableAmount( debitTxn ) {
         
         var remainingAmt = $scope.creditTxn.amount ;
         for( var i=0; i<$scope.selectedDebitTxns.length; i++ ) {
@@ -158,5 +215,32 @@ capitalystNgApp.controller( 'DebitRecoveryDialogController',
             remainingAmt = ( -1 * debitTxn.amount ) ;
         }
         return remainingAmt ;
+    }
+    
+    function validateZeroAssociations() {
+        
+        if( $scope.selectedDebitTxns.length == 0 ) {
+            var msg = "No associated debit transactions." ;
+            $scope.errorMessages.push( msg ) ;
+        }
+    }
+    
+    function validateNegativeAssociationAmounts() {
+        
+        for( var i=0; i<$scope.selectedDebitTxns.length; i++ ) {
+            var debit = $scope.selectedDebitTxns[i] ;
+            if( debit.recoveredAmount <= 0 ) {
+                var msg = "Txn " + (i+1) + " no recovered amount." ;
+                $scope.errorMessages.push( msg ) ;
+            }   
+        }
+    }
+    
+    function validateRecoveredAmountLessThanCreditAmount() {
+        
+        if( $scope.creditAmtRemaining < 0 ) {
+            var msg = "Total associated amt is more than credit amount." ;
+            $scope.errorMessages.push( msg ) ;
+        }
     }
 } ) ;
