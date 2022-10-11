17d9eb8c5755901de897d9a65f74981b6dc135a7
Sandeep Deb <deb.sandeep@gmail.com>
2022-10-11 17:38:35 +0530

    Price adjustment script

---
+++ src/main/scripts/hist-price-split-adj.sql
@@ -0,0 +1,28 @@
+SET @SPLIT_RATIO = 10 ;
+SET @SYMBOL_NSE  = 'BAJAJFINSV' collate utf8mb4_unicode_ci ;
+SET @SPLIT_DATE  = '2020-03-18' ;
+
+SELECT id, symbol, date, open, high, low, close, prev_close
+FROM historic_eq_data
+WHERE symbol = @SYMBOL_NSE
+ORDER BY date DESC ;
+
+SELECT *
+FROM historic_eq_data
+WHERE 
+    date < @SPLIT_DATE AND
+    symbol=@SYMBOL_NSE 
+ORDER BY
+    date DESC ;
+
+UPDATE historic_eq_data
+SET
+    open            = open/@SPLIT_RATIO,
+    high            = high/@SPLIT_RATIO,
+    low             = low/@SPLIT_RATIO,
+    close           = close/@SPLIT_RATIO,
+    prev_close      = prev_close/@SPLIT_RATIO,
+    total_trade_qty = total_trade_qty*@SPLIT_RATIO 
+WHERE 
+    date < @SPLIT_DATE AND
+    symbol=@SYMBOL_NSE ;
