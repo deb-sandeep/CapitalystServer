ca5aecd6376fccc4fbe1223e65c87e8d1bcdefaf
Sandeep Deb <deb.sandeep@gmail.com>
2024-02-18 14:19:53 +0530

    Querying job_run status

---
+++ src/main/java/com/sandy/capitalyst/server/dao/job/repo/JobRunEntrySpecifications.java
@@ -0,0 +1,83 @@
+package com.sandy.capitalyst.server.dao.job.repo;
+
+import com.sandy.capitalyst.server.dao.job.JobEntry;
+import com.sandy.capitalyst.server.dao.job.JobRunEntry;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Date;
+
+public class JobRunEntrySpecifications {
+
+    public static Specification<JobRunEntry> buildRootSpecification(
+            String[] jobNames,
+            String result,
+            Date fromDate, Date toDate ) {
+
+        Specification<JobRunEntry> rootSpec = Specification.where( null ) ;
+        if( jobNames != null ) {
+            rootSpec = rootSpec.and( getJobNameSpecification( jobNames ) ) ;
+        }
+
+        if( result != null && !result.equalsIgnoreCase( "any" )) {
+            rootSpec = rootSpec.and( getResultSpecification( result ) ) ;
+        }
+
+        if( fromDate != null ) {
+            rootSpec = rootSpec.and( getFromDateSpecification( fromDate ) ) ;
+        }
+
+        if( toDate != null ) {
+            rootSpec = rootSpec.and( getToDateSpecification( toDate ) ) ;
+        }
+
+        return rootSpec ;
+    }
+
+    private static Specification<JobRunEntry> getJobNameSpecification( String[] jobNames ) {
+        Specification<JobRunEntry> jobNameSpec = Specification.where( null ) ;
+        for( String jobName : jobNames ) {
+            if( jobName.startsWith( "~" ) ) {
+                jobNameSpec = jobNameSpec.or(JobRunEntrySpecifications.hasJobNameLike( jobName.substring(1) )) ;
+            }
+            else {
+                jobNameSpec = jobNameSpec.or(JobRunEntrySpecifications.hasJobName( jobName ) ) ;
+            }
+        }
+        return jobNameSpec ;
+    }
+
+    private static Specification<JobRunEntry> getFromDateSpecification( Date fromDate ) {
+        return ((root, query, criteriaBuilder) -> {
+            return criteriaBuilder.greaterThanOrEqualTo( root.get( "date" ), fromDate ) ;
+        }) ;
+    }
+
+    private static Specification<JobRunEntry> getToDateSpecification( Date toDate ) {
+        return ((root, query, criteriaBuilder) -> {
+            return criteriaBuilder.lessThanOrEqualTo( root.get( "date" ), toDate ) ;
+        }) ;
+    }
+
+    private static Specification<JobRunEntry> getResultSpecification( String result ) {
+        return ((root, query, criteriaBuilder) -> {
+            return criteriaBuilder.equal( root.get( "result" ), result ) ;
+        }) ;
+    }
+
+    private static Specification<JobRunEntry> hasJobName( String jobName ) {
+        return ((root, query, criteriaBuilder) -> {
+            Join<JobRunEntry, JobEntry> jobJoin = root.join( "job", JoinType.INNER ) ;
+            return criteriaBuilder.equal( jobJoin.get( "identity" ), jobName ) ;
+        }) ;
+    }
+
+    private static Specification<JobRunEntry> hasJobNameLike( String jobNameContains ) {
+        return ((root, query, criteriaBuilder) -> {
+            Join<JobRunEntry, JobEntry> jobJoin = root.join( "job", JoinType.INNER ) ;
+            return criteriaBuilder.like( criteriaBuilder.lower(jobJoin.get( "identity" )),
+                                         "%" + jobNameContains.toLowerCase() + "%" ) ;
+        }) ;
+    }
+}
